2fc2bb0ff2c71f934fb76a94983c5a496e732167
Elijah Oyekunle <eloyekunle@gmail.com>
2018-04-05 10:59:54 +0100

    fixed dependency issue

--- js/commerce_rave.form.js
+++ js/commerce_rave.form.js
@@ -6,14 +6,16 @@
     attach: function (context) {
       var options = drupalSettings.rave.transactionData;
 
-      $('.payment-redirect-form', context).on('submit', function () {
+      var $paymentForm = $('.payment-redirect-form', context);
+
+      $paymentForm.on('submit', function () {
         getpaidSetup(JSON.parse(options));
 
         return false;
       });
 
       // Trigger form submission when user visits Payment page.
-      $('.payment-redirect-form', context).once('getPaid').trigger('submit');
+      $paymentForm.once('getPaid').trigger('submit');
     }
   };
 
